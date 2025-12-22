a9b86df5e33a19d52eae4b2f6d78f1158dbe805b
Drakengard31 <lucdimitri31@gmail.com>
2025-12-22 20:48:41 +0100

    probleme vercel corriger avec les majuscules et minuscules

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'; // Hooks React
 import Banner from '../../components/Banner/Banner'; // Correction du chemin d'import
 import Card from '@components/Card/Card.jsx'; // Composant carte de propriété
-import './home.css'; // Styles CSS spécifiques
+import './Home.css'; // Styles CSS spécifiques
 import homeBanner from '@assets/Mask Group.png'; // Image de la bannière
 import propertiesData from '../../data/properties.json'; // Données des propriétés
 

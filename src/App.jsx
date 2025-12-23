8748ff3f162dd30ddbbc40c632099a682b4fda3f
Drakengard31 <lucdimitri31@gmail.com>
2025-12-23 18:53:11 +0100

    fix Home folder case sensitivity for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import Layout from '@components/layout/Layout.jsx'
-import Home from './pages/home/Home.jsx'
+import Home from './pages/Home/Home.jsx'
 import Property from './pages/Property/Property.jsx'
 import About from './pages/About/About.jsx'
 import NotFound from './notFound/NotFound.jsx'
